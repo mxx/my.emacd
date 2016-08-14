3d03584ec26cfc08ce3cdde7951cfe482be17eb5
kricnam <me.xinxin@gmail.com>
2016-08-14 21:21:38 +0800

    add window number support

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -9,6 +9,9 @@
 ;; The forward naming method includes part of the file's directory
 ;; name at the beginning of the buffer name
 ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Uniquify.html
+(require 'window-numbering)
+(window-numbering-mode 1)
+
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
 
